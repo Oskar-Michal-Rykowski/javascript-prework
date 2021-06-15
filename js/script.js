68a7db7f456eb26ec64d3c5ac4843d08614a62ad
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-06-15 19:27:54 +0200

    Add playerMove to printMessage

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ let computerMove = `kamień`;
 let playerMove = `papier`;
 
 printMessage(
-  'Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!'
+  'Zagrałem ' +
+    computerMove +
+    '! Jeśli Twój ruch to' +
+    playerMove +
+    ', to wygrywasz!'
 );
