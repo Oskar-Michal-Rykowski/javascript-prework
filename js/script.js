cc46a4912a4c5ae7917a0e83fd17f23fe838dd00
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-06-20 11:57:08 +0200

    Remove comments

--- js/script.js
+++ js/script.js
@@ -6,22 +6,6 @@ console.log('Ruch komputera to: ' + computerMove);
 
 let winnersMove = getWinMoveName(randomNumber);
 console.log('Ruch, bijący komputer: ' + winnersMove);
-/*
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-  winnersMove = 'papier';
-}
-
-if (randomNumber == '2') {
-  computerMove = 'papier';
-  winnersMove = 'nożyce';
-}
-
-if (randomNumber == '3') {
-  computerMove = 'nożyce';
-  winnersMove = 'kamień';
-}
-*/
 
 printMessage(
   'Zagrałem ' +
@@ -38,58 +22,11 @@ console.log('Gracz wpisał: ' + playerInput);
 let playerMove = getMoveName(playerInput);
 
 console.log('Ruch gracza to: ' + playerMove);
-/*
-if (playerInput == '1') {
-  playerMove = 'kamień';
-}
-
-if (playerInput == '2') {
-  playerMove = 'papier';
-}
-
-if (playerInput == '3') {
-  playerMove = 'nożyce';
-}
-*/
 
 printMessage('Twój ruch to: ' + playerMove);
 
 let gameResult = displayResult(playerMove, computerMove);
 
 console.log('Wynik meczu z perspektywy gracza to: ' + gameResult);
-/*
-
-let gameResult =
-  'Ups! Musisz wpisać 1, 2 lub 3, by program zadziałał poprawnie. Odśwież stronę, by spróbować jeszcze raz!';
-
-if (playerMove == 'kamień' && computerMove == 'papier') {
-  gameResult = 'Przegrana';
-}
-
-if (playerMove == 'papier' && computerMove == 'nożyce') {
-  gameResult = 'Przegrana';
-}
-
-if (playerMove == 'nożyce' && computerMove == 'kamień') {
-  gameResult = 'Przegrana';
-}
-
-if (playerMove == 'papier' && computerMove == 'kamień') {
-  gameResult = 'Wygrana';
-}
-
-if (playerMove == 'nożyce' && computerMove == 'papier') {
-  gameResult = 'Wygrana';
-}
-
-if (playerMove == 'kamień' && computerMove == 'nożyce') {
-  gameResult = 'Wygrana';
-}
-
-if (playerMove == computerMove) {
-  gameResult = 'Remis!';
-}
-
-*/
 
 printMessage('Rezultat: ' + gameResult);
